b6ca5b2da653ac790e5c291b3d3be8998ee80c42
Yonatan Lopez <granmocasin@gmail.com>
2020-11-12 12:45:07 +1100

    traduciendo

--- src/api/requestService.js
+++ src/api/requestService.js
@@ -1,18 +1,17 @@
 import axios from 'axios';
 
 /**
- * [request description]
- * @param  {[string]} url       URL of the API which needs to be consumed by client
- * @param  {[string]} method    Method type of the API call currently GET,POST,PUT,DELETE is supported in order suport to more methods add method name to the array -> allowedMethodTypes
- * @param  {[JSON]} payload     Payload to be provided to server for sending data
- * @param  {[string]} headers   Request Headers required by the server side to process the API call
- * @return {[JSON]}             Response provided by the server side code
+ * @param  {[string]} url        
+ * @param  {[string]} method     
+ * @param  {[JSON]} payload     
+ * @param  {[string]} headers   
+ * @return {[JSON]}             
  */
 function request(url, method, headers, payload) {
   return new Promise((resolve, reject) => {
-    const allowedMethodTypes = ['GET', 'POST', 'PUT', 'DELETE'];
-    if (allowedMethodTypes.indexOf(method.toLowerCase()) < 0) {
-      throw new Error(`Invalid method type please provide one of these methods... \n ${allowedMethodTypes}`);
+    const metodosPermitidos = ['GET', 'POST', 'PUT', 'DELETE'];
+    if (metodosPermitidos.indexOf(method.toLowerCase()) < 0) {
+      throw new Error(`Solo se permiten los siguientes metodos: \n ${metodosPermitidos}`);
     } else {
       axios({
         method,
@@ -21,11 +20,9 @@ function request(url, method, headers, payload) {
         headers
       })
         .then(response => {
-          // response fetched successfully
           resolve(response);
         })
         .catch(error => {
-          // error from api
           reject(error);
         });
     }
